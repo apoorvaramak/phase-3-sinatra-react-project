a29c30819d05712cd920287fdb5c952d72d9069b
Apoorva <apoorvar@comcast.net>
2021-08-23 10:45:35 -0500

    finished backend :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,87 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "epic job team :)" }.to_json
   end
+
+  get "/books" do
+    books = Book.all
+    books.to_json
+  end
+
+  get "/books/:id" do
+    book = Book.find(params[:id])
+    book.to_json(include: :reviews)
+  end
+
+  get "/users" do
+    users = User.all
+    users.to_json
+  end
+
+  get "/users/:id" do
+    user = User.find(params[:id])
+    user.to_json(include: :reviews)
+  end
+
+  get "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+  post "/books/add" do
+    book = Book.create(
+      author: params[:author],
+      title: params[:title],
+      genre: params[:genre],
+      publisher: params[:publisher],
+      page_count: params[:page_count]
+    )
+    book.to_json
+  end
+
+  post "/reviews/add" do
+    review = Review.create(
+      content: params[:content],
+      rating: params[:rating]
+    )
+    review.to_json
+  end
+
+  delete "/books/:id" do
+    book = Book.find(params[:id]).destroy_all
+    book.to_json
+  end
+
+  delete "/reviews/:id" do
+    review = Review.find(params[:id]).destroy_all
+    review.to_json
+  end
+
+  patch "/books/:id" do
+    book = Book.find(params[:id])
+    book.update(
+      author: params[:author],
+      title: params[:title],
+      genre: params[:genre],
+      publisher: params[:publisher],
+      page_count: params[:page_count]
+    )
+    book.to_json
+  end
+
+  patch "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.update(
+      content: params[:content],
+      rating: params[:rating]
+    )
+    review.to_json
+  end
+
+
+
+
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,36 @@
+require 'faker'
+
 puts "🌱 Seeding spices..."
 
+Book.destroy_all
+User.destroy_all
+Review.destroy_all
 # Seed your database here
+20.times do 
+    Book.create(
+        title: Faker::Book.title,
+        author: Faker::Book.author,
+        publisher: Faker::Book.publisher,
+        genre: Faker::Book.genre,
+        page_count: rand(100..500)
+    )
+end
+
+20.times do
+    User.create(
+        birthday: Faker::Date.birthday(min_age: 13, max_age: 99),
+        name: Faker::Name.name,
+        xp: rand(0..1000)
+    )
+end
+
+20.times do 
+    Review.create(
+        user_id: User.ids.sample,
+        book_id: Book.ids.sample,
+        content: Faker::Lorem.paragraph,
+        rating: rand(1..5)
+    )
+end
 
 puts "✅ Done seeding!"
